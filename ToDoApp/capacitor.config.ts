f539f5332d2468499c6c28d048516d0b230c8f4d
Kiran Sarode <ksarode.in@outlook.com>
2025-04-08 15:51:10 +0530

    android app using angular and ionic

---
+++ ToDoApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.todo.app',
+  appName: 'to-do-app',
+  webDir: './dist/to-do-app/'
+};
+
+export default config;
